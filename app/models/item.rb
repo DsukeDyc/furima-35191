992056856e3e24ae013b3b793641c9d968318893
DsukeDyc <dsuke_dyc@yahoo.co.jp>
2021-04-24 07:18:36 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,25 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :type_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipdate_id
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :type_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id 
+    validates :shipdate_id
+    validates :price
+  end
+
+  with_options numericality: {other_than: 1 } do
+    validates :type_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id 
+    validates :shipdate_id
 end
