60b3fe5746af6c556821dae76ccb831173f1110a
DsukeDyc <dsuke_dyc@yahoo.co.jp>
2021-04-26 10:27:23 +0900

    データの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :type_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipdate_id, :price, :user_id, :image)
+    params.require(:item).permit(:name, :description, :type_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipdate_id, :price, :image).merge(user_id: current_user.id)
   end
 end
